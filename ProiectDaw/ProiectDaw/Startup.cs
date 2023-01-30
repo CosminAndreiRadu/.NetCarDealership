e5b900a213103fff297736b7152508d5a31e1a85
CosminAndreiRadu <80643204+CosminAndreiRadu@users.noreply.github.com>
2023-01-30 02:17:15 +0200

    finished 3 CRUD controllers + linq methods

--- ProiectDaw/ProiectDaw/Startup.cs
+++ ProiectDaw/ProiectDaw/Startup.cs
@@ -2,21 +2,28 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using ProiectDaw.Data;
 using ProiectDaw.Models.Constants;
+using ProiectDaw.Models.Entities;
 using ProiectDaw.Repositories;
+using ProiectDaw.Repositories.LocationRepository;
 using ProiectDaw.Repositories.ManufacturerRepository;
+using ProiectDaw.Repositories.VehicleRepository;
+using ProiectDaw.Seed;
 using ProiectDaw.Services.UserServices;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace ProiectDaw
@@ -43,6 +50,10 @@ namespace ProiectDaw
 
             services.AddDbContext<DBcon>(options => options.UseSqlServer("Data Source=DESKTOP-GPUB6TL\\SERVERDAW;Initial Catalog=DAWDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False"));
 
+            services.AddIdentity<User, Role>()
+                .AddEntityFrameworkStores<DBcon>()
+                .AddDefaultTokenProviders();
+
             services.AddScoped<IUserService, UserService>();
 
             services.AddAuthorization(options =>
@@ -58,11 +69,35 @@ namespace ProiectDaw
                 auth.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
                 auth.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
             })
-            .AddJwtBearer();
+            .AddJwtBearer( options =>
+            {
+                options.SaveToken= true;
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    ValidateLifetime = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("this is my custom secret key for authentication")),
+                    ValidateIssuerSigningKey = true
+
+                };
+                options.Events = new JwtBearerEvents()
+                {
+                    OnTokenValidated = Helpers.SessionTokenValidator.ValidateSessionToken
+                };
+            });
+
+
 
             services.AddScoped<IRepositoryWrapper, RepositoryWrapper>();
 
             services.AddTransient<IManufacturerRepository, ManufacturerRepository>();
+
+            services.AddTransient<IVehicleRepository, VehicleRepository>();
+
+            services.AddTransient<ILocationRepository, LocationRepository>();
+
+            services.AddScoped<SeedDb>();
 
             services.AddControllersWithViews()
                 .AddNewtonsoftJson(options =>
@@ -71,7 +106,7 @@ namespace ProiectDaw
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app,SeedDb seed, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
@@ -84,12 +119,23 @@ namespace ProiectDaw
 
             app.UseRouting();
 
+            app.UseAuthentication();
+
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
             });
+
+            try
+            {
+                seed.SeedRoles().Wait();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
