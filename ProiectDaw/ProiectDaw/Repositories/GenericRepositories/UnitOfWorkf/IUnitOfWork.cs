ce3c0010437fd6783a4a0c7c9f6db69adc46665d
CosminAndreiRadu <80643204+CosminAndreiRadu@users.noreply.github.com>
2023-02-04 19:37:05 +0200

    added unit of work

---
+++ ProiectDaw/ProiectDaw/Repositories/GenericRepositories/UnitOfWorkf/IUnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using ProiectDaw.Data;
+
+namespace ProiectDaw.UnitOfWorkf
+{
+    public interface IUnitOfWork<out TContext> where TContext : DBcon, new()
+    {
+        TContext Context { get; }
+        void CreateTransaction();
+        void Commit();
+        void Rollback();
+        void Save();
+
+    }
+}
